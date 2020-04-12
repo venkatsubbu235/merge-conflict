5559e478a412e38c7a067e9d561f9ccd43860dfa
subbu <nvvsubrahmanyam235@gmail.com>
2020-04-12 13:41:00 +0000

    feature2

--- main.c
+++ main.c
@@ -8,14 +8,14 @@ Initial Function()
 Initial Security()
 {
  //Sample Code
-=======
-feature1()
+}
+
+feaature2()
 {
-  //Sample Code for Feature 1
-  //>>>>>>> added feature A
-}
-
-
+  //Sample Code for Feature 2
 
 }
 
+
+
+}
